f7d0ab5850480dd8fa36ee9c37014ca3514a3174
Fenton Martin <fen.official@gmail.com>
2017-06-23 06:17:48 +0700

    Add startLoop

--- app/src/main/java/fen/code/loopingactivity/MainActivity.java
+++ app/src/main/java/fen/code/loopingactivity/MainActivity.java
@@ -1,7 +1,10 @@
 package fen.code.loopingactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import fen.code.loopingactivity.utils.Strings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
         int number = 0;
         int max = 10;
 
+    }
+
+    private void startLoop(int number, int max) {
+        /* init intent to LoopActivity */
+        Intent intent = new Intent(MainActivity.this, LoopActivity.class);
+        /* init Extra INT to intent */
+        intent.putExtra(Strings.EXTRA_INT, number);
+        /* init Extra MAX to intent */
+        intent.putExtra(Strings.EXTRA_MAX, max);
+        /* start intent activity */
+        startActivity(intent);
     }
 }
