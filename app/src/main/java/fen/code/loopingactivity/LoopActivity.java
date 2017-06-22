6a0698324ed6a373ad4ff157e7fd117867a1b44e
Fenton Martin <fen.official@gmail.com>
2017-06-23 06:29:54 +0700

    Init Number ans Max using getIntExtra from intent

--- app/src/main/java/fen/code/loopingactivity/LoopActivity.java
+++ app/src/main/java/fen/code/loopingactivity/LoopActivity.java
@@ -1,13 +1,23 @@
 package fen.code.loopingactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fen.code.loopingactivity.utils.Strings;
 
 public class LoopActivity extends AppCompatActivity {
+
+    int number;
+    int max;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loop);
+
+        /* init number using getIntExtra from intent default 0*/
+        number = getIntent().getIntExtra(Strings.EXTRA_INT, 0);
+        /* init max using getIntExtra from intent default 1*/
+        max = getIntent().getIntExtra(Strings.EXTRA_MAX, 1);
     }
 }
