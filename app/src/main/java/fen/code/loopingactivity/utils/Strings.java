deb89b0a620afae07daa063f12821602c62a6bcd
Fenton Martin <fen.official@gmail.com>
2017-06-23 06:08:40 +0700

    Add Strings Utils

---
+++ app/src/main/java/fen/code/loopingactivity/utils/Strings.java
@@ -0,0 +1,12 @@
+package fen.code.loopingactivity.utils;
+
+/**
+ * Created by FEN on 23/06/2017.
+ */
+
+public class Strings {
+
+    public static String EXTRA_INT = "extra_int";
+    public static String EXTRA_MAX = "extra_max";
+    
+}
