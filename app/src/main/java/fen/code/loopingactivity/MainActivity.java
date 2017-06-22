eb761f269327be57f69597a6d94850ad390dd690
Fenton Martin <fen.official@gmail.com>
2017-06-23 06:27:08 +0700

    Add setOnClickListener on Button

--- app/src/main/java/fen/code/loopingactivity/MainActivity.java
+++ app/src/main/java/fen/code/loopingactivity/MainActivity.java
@@ -3,6 +3,8 @@ package fen.code.loopingactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import fen.code.loopingactivity.utils.Strings;
 
@@ -17,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         int number = 0;
         int max = 10;
 
+        /* declare buttton */
+        Button button = (Button) findViewById(R.id.button);
+        /* add setOnClickListener */
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void startLoop(int number, int max) {
