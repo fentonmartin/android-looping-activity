c71ad56c8dac51f4692302573a4da488224add18
Fenton Martin <fen.official@gmail.com>
2017-06-23 06:29:21 +0700

    Call startLoop on OnClick

--- app/src/main/java/fen/code/loopingactivity/MainActivity.java
+++ app/src/main/java/fen/code/loopingactivity/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                /* call startLoop */
+                startLoop(number, max);
             }
         });
     }
